3ee150dc4aa2e3098a1898a0a77d820ef7a96c2f
MatheusDevelop <mathvierabarbosa@gmail.com>
2023-10-19 10:22:20 -0300

    feat: feedback alert

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,10 @@ import useSWR from "swr";
 import axios from "axios";
 import useTableData from "@/hooks/useTableData";
 import Table from "../components/Table";
-import { Backdrop, CircularProgress } from "@mui/material";
+import { Alert, Backdrop, CircularProgress } from "@mui/material";
 
 export default function Home() {
   const { tableData, error } = useTableData();
-  if (error) return <div>Ocorreu um erro ao carregar os dados</div>;
   const columns = [
     { field: "numero", headerName: "Nº", width: 50 },
     { field: "campo", headerName: "Campo", width: 150 },
@@ -20,6 +19,13 @@ export default function Home() {
 
   return (
     <div className="h-screen w-screen p-5 bg-white">
+      {error && (
+        <div className="flex fixed z-30 top-0 left-0">
+          <Alert severity="error" className="w-screen">
+            Ocorreu um erro ao carregar os dados
+          </Alert>
+        </div>
+      )}
       <Backdrop sx={{ color: "#fff", zIndex: 99 }} open={!tableData}>
         <CircularProgress color="inherit" />
       </Backdrop>
